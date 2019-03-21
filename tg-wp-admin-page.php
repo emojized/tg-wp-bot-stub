90aa55c8ff625c9ef2b7f3845862f371974b8eb2
theo <theo@Theos-MacBook-Pro.local>
2019-03-21 22:55:15 +0100

    Debug dumps remove

--- tg-wp-admin-page.php
+++ tg-wp-admin-page.php
@@ -36,6 +36,7 @@ function render_tg_wp_admin_page()
 	}
 	
 	echo '<pre>';
+		var_dump(tg_wp_check_if_is_wp_telegram_login_user());
 		var_dump(tg_wp_user_is_alowed_to_use_bot("197094729"));
 	echo '</pre>';
 	
